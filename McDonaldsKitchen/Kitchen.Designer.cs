01e709138372366493aede604c869654ce0b9c8a
alparslanKobak <alparslankobak@gmail.com>
2023-12-06 22:21:42 +0300

    GroupBox created

--- McDonaldsKitchen/Kitchen.Designer.cs
+++ McDonaldsKitchen/Kitchen.Designer.cs
@@ -28,54 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.headerPanel = new System.Windows.Forms.Panel();
             this.titleLabel = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.headerPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // headerPanel
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel1.Controls.Add(this.titleLabel);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1606, 100);
-            this.panel1.TabIndex = 0;
+            this.headerPanel.AutoSize = true;
+            this.headerPanel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.headerPanel.Controls.Add(this.titleLabel);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(1613, 84);
+            this.headerPanel.TabIndex = 0;
             // 
             // titleLabel
             // 
             this.titleLabel.AutoSize = true;
-            this.titleLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.titleLabel.Location = new System.Drawing.Point(673, 9);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(285, 82);
+            this.titleLabel.Size = new System.Drawing.Size(257, 75);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "ORDERS";
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoScroll = true;
+            this.mainPanel.AutoSize = true;
+            this.mainPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 84);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1613, 537);
+            this.mainPanel.TabIndex = 1;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
             // 
             // Kitchen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(1606, 621);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1613, 621);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.headerPanel);
             this.Name = "Kitchen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Kitchen_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.SizeChanged += new System.EventHandler(this.Kitchen_SizeChanged);
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel headerPanel;
         private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
 
--- McDonaldsKitchen/Kitchen.cs
+++ McDonaldsKitchen/Kitchen.cs
@@ -12,14 +12,149 @@ namespace McDonaldsKitchen
 {
     public partial class Kitchen : Form
     {
+
+
+        List<Order> orders = new List<Order>();
         public Kitchen()
         {
             InitializeComponent();
+
+            // Daha fazla sipariş ekleyebilirsiniz...
+            this.MinimumSize = new Size(290, this.MinimumSize.Height);
+
         }
 
         private void Kitchen_Load(object sender, EventArgs e)
         {
+            orders.Add(new Order() { Id = 1, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 2, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 3, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 4, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 5, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 6, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+
+            orders.Add(new Order() { Id = 1, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 2, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 3, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 4, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 5, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 6, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+
+            orders.Add(new Order() { Id = 1, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 2, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 3, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 4, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 5, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 6, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+
+            orders.Add(new Order() { Id = 1, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 2, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 3, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 4, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 5, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+            orders.Add(new Order() { Id = 6, OrderName = "Big Mac", OrderPrice = "$5", OrderStatus = "Hazırlanıyor" });
+
+            
+
+
+            DisplayOrders();
+        }
+
+        private void DisplayOrders()
+        {
+            mainPanel.Controls.Clear(); // Mevcut kontrolleri temizle
+
+            // GroupBox ölçülerini ve aralıkları tanımla
+            int groupBoxWidth = 200; // GroupBox genişliği
+            int groupBoxHeight = 250; // GroupBox yüksekliği
+            int horizontalSpacing = 30; // Yatay aralığı genişlet
+            int verticalSpacing = 30; // Dikey aralığı genişlet
+            int marginTop = 40; // İlk satır için üst boşluk
+            int margin = 20; // İlk GroupBox için sol boşluk
+
+            // Maksimum GroupBox sayısını pencere genişliğine göre belirle
+            int groupBoxesPerRow;
+            if (mainPanel.Width < this.MinimumSize.Width)
+            {
+                groupBoxesPerRow = 1;
+            }
+            else
+            {
+                groupBoxesPerRow = this.WindowState == FormWindowState.Maximized ? 8 : (mainPanel.Width - margin) / (groupBoxWidth + horizontalSpacing);
+                if (groupBoxesPerRow == 0) groupBoxesPerRow = 1; // Sıfırdan kaçınmak için kontrol et
+            }
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                // GroupBox oluştur ve özelliklerini ayarla
+                GroupBox groupBox = new GroupBox
+                {
+                    Text = orders[i].OrderName,
+                    Size = new Size(groupBoxWidth, groupBoxHeight),
+                    Font = new Font("Microsoft Tai Le", 14),
+                    BackColor = Color.White,
+                    AccessibilityObject = { Name = orders[i].Id.ToString() }
+                };
+
+                // GroupBox konumunu hesapla
+                int row = i / groupBoxesPerRow; // Her satır için GroupBox sayısını hesapla
+                int column = i % groupBoxesPerRow; // Her satırda kaçıncı GroupBox olduğunu hesapla
+                int x = column * (groupBoxWidth + horizontalSpacing) + margin; 
+                int y = row * (groupBoxHeight + verticalSpacing) + marginTop; 
+
+                // Her satırın ilk GroupBox'ı için sol boşluğu ayarla
+                if (column == 0) x = margin;
+
+                groupBox.Location = new Point(x, y);
+
+                // Sipariş detayları için Label'lar ekle
+                Label priceLabel = new Label
+                {
+                    Text = "Price: " + orders[i].OrderPrice,
+                    Location = new Point(10, 20),
+                    Size = new Size(groupBoxWidth - 20, 20)
+                };
+
+                Label statusLabel = new Label
+                {
+                    Text = "Status: " + orders[i].OrderStatus,
+                    Location = new Point(10, 40),
+                    Size = new Size(groupBoxWidth - 20, 20)
+                };
+
+                // Label'ları GroupBox'a ekle
+                groupBox.Controls.Add(priceLabel);
+                groupBox.Controls.Add(statusLabel);
+
+                // GroupBox'ı mainPanel'e ekle
+                mainPanel.Controls.Add(groupBox);
+            }
+        }
+
+        private void Kitchen_SizeChanged(object sender, EventArgs e)
+        {
+            // headerPanel'in genişliğinin ortasını hesapla, ardından titleLabel'ın yarısını çıkararak titleLabel'ı ortala
+            int formThisMiddleWidth = (headerPanel.Width / 2) - (titleLabel.Width / 2);
+
+            // headerPanel'in yüksekliğinin ortasını hesapla, ardından titleLabel'ın yarısını çıkararak titleLabel'ı dikey olarak ortala
+            int formHeight = (headerPanel.Height / 2) - (titleLabel.Height / 2);
+
+            // titleLabel'ın yeni konumunu ayarla
+            titleLabel.Location = new Point(formThisMiddleWidth, formHeight);
+            DisplayOrders();
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
 
         }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+        public string OrderPrice { get; set; }
+        public string OrderStatus { get; set; }
     }
 }
